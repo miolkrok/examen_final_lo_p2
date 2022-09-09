f3122bddfce7476b25d2e30f74b1ab2ecf916604
Luis Ortiz <luchoaam97@gmail.com>
2022-09-09 18:07:07 -0500

    segundo commit examen final

---
+++ src/main/java/com/uce/edu/demo/ExamenFinalLoP2Application.java
@@ -0,0 +1,13 @@
+package com.uce.edu.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenFinalLoP2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenFinalLoP2Application.class, args);
+	}
+
+}
